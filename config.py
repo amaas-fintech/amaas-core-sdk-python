6fe9c2ae61c4e67ec5d42511627868d66b64ce9a
Ned Lowe <ned@amaas.com>
2017-01-29 12:23:25 +0800

    Support Monitor from Python SDK.  AMAAS-392.

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ if LOCAL:
     ENDPOINTS = {
         'assets': 'http://localhost:8000',
         'books': 'http://localhost:8000',
+        'monitor': 'http://localhost:8000',
         'parties': 'http://localhost:8000',
         'transactions': 'http://localhost:8000'
     }
@@ -13,6 +14,7 @@ else:
     ENDPOINTS = {
         'assets': 'https://zc6udsq1nb.execute-api.ap-southeast-1.amazonaws.com/dev',
         'books': 'https://smc367plfg.execute-api.ap-southeast-1.amazonaws.com/dev',
+        'monitor': 'https://wt50nd7j7l.execute-api.ap-southeast-1.amazonaws.com/dev',
         'parties': 'https://hpihgzmxoc.execute-api.ap-southeast-1.amazonaws.com/dev',
         'transactions': 'https://1w0gb581sl.execute-api.ap-southeast-1.amazonaws.com/dev'
     }
