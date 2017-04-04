98ba687e67c8d5a17560bed59f42dbe8e3fb0cf6
Ned Lowe <ned@amaas.com>
2017-04-04 19:28:57 +0800

    Remove Individual as a  book_type - it doesn’t really add anything.  AMAAS-639.

--- amaascore/books/enums.py
+++ amaascore/books/enums.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-BOOK_TYPES = {'Counterparty', 'Individual', 'Management', 'Trading', 'Wash'}
+BOOK_TYPES = {'Counterparty', 'Management', 'Trading', 'Wash'}
