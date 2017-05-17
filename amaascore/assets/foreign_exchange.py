3149eeda0e766c2e2d71e1e1d4a969ea57492b5a
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-17 16:57:30 +0800

    foreign exchange - major added

--- amaascore/assets/foreign_exchange.py
+++ amaascore/assets/foreign_exchange.py
@@ -6,9 +6,10 @@ from amaascore.assets.asset import Asset
 class ForeignExchangeBase(Asset):
     """ This class should never be instantiated """
 
-    def __init__(self, asset_id, asset_status, description, country_codes, *args, **kwargs):
+    def __init__(self, asset_id, asset_status, description, major, country_codes, *args, **kwargs):
         self.asset_class = 'ForeignExchange'
         self.country_codes = country_codes
+        self.major = major
         super(ForeignExchangeBase, self).__init__(asset_manager_id=0, asset_id=asset_id, fungible=True,
                                                   display_name=asset_id, roll_price=False,
                                                   asset_status=asset_status, description=description,
@@ -28,9 +29,9 @@ class ForeignExchange(ForeignExchangeBase):
     Currently modelling spot and forward as the same, just two different dates on the transaction.  We might need to
     change that.
     """
-    def __init__(self, asset_id, asset_status='Active', country_codes=[], description='', *args, **kwargs):
+    def __init__(self, asset_id, asset_status='Active', country_codes=[], major=False, description='', *args, **kwargs):
         super(ForeignExchange, self).__init__(asset_id=asset_id, asset_status=asset_status, description=description,
-                                              country_codes=country_codes,
+                                              country_codes=country_codes, major=major,
                                               *args, **kwargs)
 
 
@@ -40,7 +41,7 @@ class NonDeliverableForward(ForeignExchangeBase):
     change that.
     """
 
-    def __init__(self, asset_id, asset_status='Active', description='', country_codes=[], *args, **kwargs):
+    def __init__(self, asset_id, asset_status='Active', description='', major=False, country_codes=[], *args, **kwargs):
         super(NonDeliverableForward, self).__init__(asset_id=asset_id, asset_status=asset_status, country_codes=country_codes,
-                                                    description=description, *args, **kwargs)
+                                                    major=major, description=description, *args, **kwargs)
 
