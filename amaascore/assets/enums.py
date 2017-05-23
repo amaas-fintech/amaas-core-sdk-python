2a52d299fbd59904139790840e21e9bca920987c
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-23 18:18:47 +0800

    update enum file; add in warrant and automobile classes to asset

--- amaascore/assets/enums.py
+++ amaascore/assets/enums.py
@@ -3,3 +3,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 WINE_CLASSIFICATIONS = {'1er Cru', '1er Cru Classé', '1er Grand Cru', '2ème Cru Classé', '2ème Vin',
                         '4ème Cru Classé', 'Cru Classé', 'Grand Cru', 'Non Classé'}
 WINE_PACKING_TYPE = {'OCB06', 'OCB12', 'OWC06', 'OWC01', 'OWC02', 'OWC03', 'OWC06', 'OWC12'}
+
+CAR_COLOR_RECORD = {'White', 'Silver', 'Black', 'Grey', 'Blue', 'Red', 'Brown', 'Green'}
+CAR_MAKE_RECORD = {'McLaren', 'Koenigsegg', 'Bugatti', 'Pagani', 'Aston Martin', 'Mercedes', 'Jaguar', 'Porsche',
+            'Ferrari', 'Lamborghini', 'Rolls Royce', 'Bentley', 'Maybach', 'Audi', 'Maserati', 'Lexus', 'BMW', 'Cadillac'}
+CAR_STYLE = {'R': 'Roadster', 'CV': 'Convertible', 'CP': 'Coupe', 'S': 'Sedan', 'SUV': 'Sport utility vehicle'}
+CAR_GENRE = {'M': 'Modern', 'C': 'Classic', 'A': 'Antique', 'E': 'Exotic'}
+CAR_RARITY = {'super rare', 'very rare', 'rare', 'fairly rare', None}
+CAR_CONDITION = {'DC': 'Driving Condition', 'SS': 'Street Show Condition', 'SC': 'Show Car', 'CC': 'Concours Condition'}
+CAR_PETROL_GRADE = {87: 'regular', 88: 'midgrade', 89: 'midgrade',
+                    90: 'midgrade', 91: 'premium', 92: 'premium', 93: 'premium', 94: 'premium'}
