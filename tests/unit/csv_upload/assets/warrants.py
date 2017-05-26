f1a5f64fe17480dbb053917453ea1a56e707f230
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-26 10:12:27 +0800

    fix the circular dependent import problem

--- tests/unit/csv_upload/assets/warrants.py
+++ tests/unit/csv_upload/assets/warrants.py
@@ -3,22 +3,22 @@ import csv
 import random
 
 from amaasutils.random_utils import random_string
-from amaascore.csv_upload.assets.warrant import WarrantUploader
+from amaascore.csv_upload.assets.warrant import *
 from amaascore.assets.warrant import Warrant
 from amaascore.tools.csv_tools import objects_to_csv_stream
 
-class EquityUploaderTest(unittest.TestCase):
+class WarrantUploaderTest(unittest.TestCase):
 
     def setUp(self):
         self.longMessage = True  # Print complete error message on failure
         self.asset_manager_id = self.client_id = 1
-        self.csvfile = 'AutomobileUploaderTest.csv'
+        self.csvfile = 'WarrantUploaderTest.csv'
         self.asset_id = 'AGDJSA2T'
 
     def tearDown(self):
         pass
 
-    def test_AutomobileUploadDownload(self):
+    def test_WarrantUploadDownload(self):
         WarrantUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile)
         WarrantUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=[self.asset_id])
 
