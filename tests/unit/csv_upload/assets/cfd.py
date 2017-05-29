7f8fcc6eccbeaaad290d221f74582ea19c8f9aa7
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-29 16:39:01 +0800

    update unittest for cfd upload

--- tests/unit/csv_upload/assets/cfd.py
+++ tests/unit/csv_upload/assets/cfd.py
@@ -13,14 +13,18 @@ class CFDUploaderTest(unittest.TestCase):
         self.longMessage = True  # Print complete error message on failure
         self.asset_manager_id = self.client_id = 1
         self.csvfile = 'CFDUploaderTest.csv'
-        self.asset_id = 'GD1DISR12'
+        #self.asset_id = 'GD1DISR12'
+        self.asset_ids = []
+        for i in range(2):
+            self.asset_ids.append(random_string(8))
 
     def tearDown(self):
         pass
 
     def test_CFDUploadDownload(self):
-        CFDUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile)
-        CFDUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=[self.asset_id])
+        for asset_id in self.asset_ids:
+            CFDUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile, asset_id=asset_id)
+        CFDUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=self.asset_ids)
 
 if __name__ == '__main__':
     unittest.main()
