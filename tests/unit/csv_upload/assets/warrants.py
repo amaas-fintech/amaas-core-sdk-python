ebb746ad4802474408abd8d485d9ea1ace77b0b2
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-29 16:14:17 +0800

    update unittest for warrants upload

--- tests/unit/csv_upload/assets/warrants.py
+++ tests/unit/csv_upload/assets/warrants.py
@@ -13,14 +13,18 @@ class WarrantUploaderTest(unittest.TestCase):
         self.longMessage = True  # Print complete error message on failure
         self.asset_manager_id = self.client_id = 1
         self.csvfile = 'WarrantsUploaderTest.csv'
-        self.asset_id = 'AGDJSA2T'
+        #self.asset_id = 'AGDJSA2T'
+        self.asset_ids = []
+        for i in range(2):
+                self.asset_ids.append(random_string(8))
 
     def tearDown(self):
         pass
 
     def test_WarrantUploadDownload(self):
-        WarrantUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile)
-        WarrantUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=[self.asset_id])
+        for asset_id in self.asset_ids:
+            WarrantUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile, asset_id=asset_id)
+        WarrantUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=self.asset_ids)
 
 if __name__ == '__main__':
     unittest.main()
