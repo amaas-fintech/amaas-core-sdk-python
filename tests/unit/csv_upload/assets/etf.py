ced62059628db975ff28e978b92d6de26b95f15a
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-29 16:03:46 +0800

    update unittest for etf upload

--- tests/unit/csv_upload/assets/etf.py
+++ tests/unit/csv_upload/assets/etf.py
@@ -13,14 +13,17 @@ class ETFUploaderTest(unittest.TestCase):
         self.longMessage = True  # Print complete error message on failure
         self.asset_manager_id = self.client_id = 1
         self.csvfile = 'ETFUploaderTest.csv'
-        self.asset_id = 'RANDOMA'
-
+        #self.asset_id = random_string(8)
+        self.asset_ids = []
+        for i in range(2):
+            self.asset_ids.append(random_string(8))
     def tearDown(self):
         pass
 
     def test_ETFUploadDownload(self):
-        ETFUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile)
-        ETFUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=[self.asset_id])
+        for asset_id in self.asset_ids:
+            ETFUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile, asset_id=asset_id)
+        ETFUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=self.asset_ids)
 
 if __name__ == '__main__':
     unittest.main()
