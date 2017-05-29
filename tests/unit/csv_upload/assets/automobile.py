7235b12de7c219568b12eaf723154d97e60cac1d
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-29 16:43:44 +0800

    update unittest for automobile upload

--- tests/unit/csv_upload/assets/automobile.py
+++ tests/unit/csv_upload/assets/automobile.py
@@ -13,14 +13,18 @@ class EquityUploaderTest(unittest.TestCase):
         self.longMessage = True  # Print complete error message on failure
         self.asset_manager_id = self.client_id = 1
         self.csvfile = 'AutomobileUploaderTest.csv'
-        self.asset_id = 'US9IZ3235T'
-
+        #self.asset_id = 'US9IZ3235T'
+        self.asset_ids = []
+        for i in range(2):
+            self.asset_ids.append(random_string(8))
+        
     def tearDown(self):
         pass
 
     def test_AutomobileUploadDownload(self):
-        AutomobileUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile)
-        AutomobileUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=[self.asset_id])
+        for asset_id in self.asset_ids:
+            AutomobileUploader().upload(asset_manager_id=self.asset_manager_id, client_id=self.client_id, csvpath=self.csvfile, asset_id=asset_id)
+        AutomobileUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=self.asset_ids)
 
 if __name__ == '__main__':
     unittest.main()
