f9d1e47f1a4c80a659e7fe3858407ab0e69cd9c2
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-05-31 10:07:07 +0800

    add generic csv upload function and its helper enums file

---
+++ amaascore/csv_upload/enums.py
@@ -0,0 +1,29 @@
+import csv
+
+from amaascore.tools.csv_tools import csv_stream_to_objects
+from amaasutils.logging_utils import DEFAULT_LOGGING
+
+from amaascore.assets.interface import AssetsInterface
+from amaascore.parties.interface import PartiesInterface
+from amaascore.books.interface import BooksInterface
+
+ASSET = ['Asset', 'Automobile', 'BondFutureOption', 'BondFuture', 'BondOption', 'Bond', 'ContractForDifference', 'Currency', 'CustomAsset', 
+         'Derivative', 'EnergyFuture', 'EquityFuture', 'Equity', 'ExchangeTradedFund', 'ForeignExchange', 'Fund', 'FutureOption', 'Future'
+         'ForeignExchangeOption', 'IndexFuture', 'Index', 'InterestRateFuture', 'ListedContractForDifference', 'ListedDerivative'
+         'OptionMixin', 'RealAsset', 'RealEstate', 'Sukuk', 'SyntheticFromBook', 'SyntheticMultiLeg', 'Synthetic', 'Warrant', 'Wine']
+PARTY = ['Broker', 'Company', 'Exchange', 'Fund', 'GovernmentAgency', 'Individual', 'Organisation', 'Party', 'SubFund']
+BOOK = ['Book']
+
+def interface_direct (csvpath):
+    with open(csvpath, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data_class = row.get('amaasclass')
+            break
+    if data_class in ASSET:
+        interface = AssetsInterface()
+    elif data_class in PARTY:
+        interface = PartiesInterface()
+    else:
+        interface = BooksInterface()
+    return interface
