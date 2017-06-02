fd0c8a550eb6707446b77ceb1a6a60de5958149d
DESKTOP-6ONSCGU\shaocong.dong <shaocong.dong@amaas.com>
2017-06-02 18:42:41 +0800

    update equity upload testcase

--- tests/unit/csv_upload/assets/equity.py
+++ tests/unit/csv_upload/assets/equity.py
@@ -3,7 +3,7 @@ import csv
 import random
 
 from amaasutils.random_utils import random_string
-from amaascore.csv_upload.data import Uploader
+from amaascore.csv_upload.assets.asset import AssetUploader
 
 class EquityUploaderTest(unittest.TestCase):
 
@@ -20,16 +20,19 @@ class EquityUploaderTest(unittest.TestCase):
         with open(self.csvfile, 'w+', newline='') as writefile:
             writer = csv.writer(writefile)
             writer.writerow(header)
-            writer.writerow(['Equity', self.asset_ids[0], '123', '123', '123', '123', '123', '123', '123', '123', '123', '123','',''])
-            writer.writerow(['Equity', self.asset_ids[1], '123', '123', '123', '123', '123', '123', '123', '123', '123', '123','',''])
-
+            writer.writerow(['Equity', self.asset_ids[0], '123', '123', '123', '123', '123', '123', '123', '123', '123', '123',
+                             '{link_1:[{linked_asset_id:12345},{linked_asset_id:54321,active:true}],link_2:[{linked_asset_id:12365}]}', 
+                             '{reference_1:{reference_value:1,active:true},reference_2:{reference_value:2}}'])
+            writer.writerow(['Equity', self.asset_ids[1], '123', '123', '123', '123', '123', '123', '123', '123', '123', '123',
+                             '{link_1:[{linked_asset_id:12345},{linked_asset_id:54321,active:true}],link_2:[{linked_asset_id:12365}]}', 
+                             '{reference_1:{reference_value:1,active:true},reference_2:{reference_value:2}}'])
 
     def tearDown(self):
         pass
 
-    def test_PartyUploadDownload(self):
-        Uploader().upload(csvpath=self.csvfile, asset_manager_id=self.asset_manager_id, client_id=self.client_id)
-        Uploader().download(csvpath=self.csvfile, asset_manager_id=self.asset_manager_id, data_id_type='asset_id', data_id_list=self.asset_ids)
+    def test_EquityUploadDownload(self):
+        AssetUploader().upload(csvpath=self.csvfile, asset_manager_id=self.asset_manager_id, client_id=self.client_id)
+        AssetUploader().download(asset_manager_id=self.asset_manager_id, asset_id_list=self.asset_ids)
 
 if __name__ == '__main__':
     unittest.main()
