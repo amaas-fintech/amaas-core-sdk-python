998f1726362d9b35a81da6640cb7de2c15cad706
paul-rs <roland.paul.santos@gmail.com>
2017-07-24 12:33:53 +0800

    AMAAS-957 Added eod books get in sdk

--- amaascore/tools/generate_asset_manager.py
+++ amaascore/tools/generate_asset_manager.py
@@ -4,6 +4,7 @@ from amaasutils.random_utils import random_string
 import random
 
 from amaascore.asset_managers.asset_manager import AssetManager
+from amaascore.asset_managers.eod_book import EODBook
 from amaascore.asset_managers.enums import ASSET_MANAGER_TYPES, RELATIONSHIP_TYPES
 from amaascore.asset_managers.relationship import Relationship
 
@@ -24,3 +25,9 @@ def generate_relationship(asset_manager_id=None, client_id=None, related_id=None
                                 relationship_type=relationship_type or random.choice(list(RELATIONSHIP_TYPES)),
                                 related_id=related_id or random.randint(1, 2**31-1))
     return relationship
+
+def generate_eod_book(asset_manager_id=None, book_id=None, utc_close_time='18:00:00'):
+    eod_book = EODBook(asset_manager_id=asset_manager_id, 
+                       book_id=book_id or str(random.randint(1, 2**31-1)),
+                       utc_close_time=utc_close_time)
+    return eod_book
