a26eda36a10682822ef35c836f6f05328a9de008
paul-rs <roland.paul.santos@gmail.com>
2017-09-08 11:06:41 +0800

    AMAAS-1261 Added new relationship type

--- amaascore/asset_managers/enums.py
+++ amaascore/asset_managers/enums.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 ASSET_MANAGER_TYPES = {'Accredited Investor', 'Bank', 'Broker', 'Corporate Treasury', 'Family Office',
                        'Fund Administrator', 'Fund Manager', 'Hedge Fund', 'Individual', 'Private Equity',
                        'Venture Capital', 'Data Provider'}
-RELATIONSHIP_TYPES = {'Administrator', 'External', 'Front Office', 'Employee'}
+RELATIONSHIP_TYPES = {'Administrator', 'External', 'Front Office', 'Employee', 'Data Provider'}
 ACCOUNT_TYPES = {'Internal', 'Test', 'Basic', 'Professional'}
