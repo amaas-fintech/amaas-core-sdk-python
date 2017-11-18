96ca726b2e96be91567fb2d09f2666b5645bf5ed
paul-rs <roland.paul.santos@gmail.com>
2017-11-18 14:00:16 +0800

    AMAAS-1186 Fix hash differences

--- amaascore/assets/fx_future.py
+++ amaascore/assets/fx_future.py
@@ -12,7 +12,7 @@ class ForeignExchangeFuture(Future):
                  quote_unit=None, asset_issuer_id=None, asset_status='Active', issue_date=date.min,
                  expiry_date=date.max, currency=None, underlying_asset_id=None,
                  display_name='', description='', country_id=None, venue_id=None,
-                 links=None, references=None, *args, **kwargs):
+                 comments=None, links=None, references=None, *args, **kwargs):
         """
 
         :param asset_manager_id: The asset manager who owns the data for this ForeignExchangeFuture
@@ -30,12 +30,12 @@ class ForeignExchangeFuture(Future):
         :param kwargs:
         """
         super(ForeignExchangeFuture, self).__init__(asset_manager_id=asset_manager_id, asset_id=asset_id,
-                                           settlement_type=settlement_type, contract_size=contract_size,
-                                           point_value=point_value, tick_size=tick_size, quote_unit=quote_unit,
-                                           asset_issuer_id=asset_issuer_id, asset_status=asset_status,
-                                           display_name=display_name, currency=currency,
-                                           underlying_asset_id=underlying_asset_id,
-                                           description=description, country_id=country_id, venue_id=venue_id,
-                                           links=links, references=references, issue_date=issue_date,
-                                           expiry_date=expiry_date,
-                                           *args, **kwargs)
+                                                    settlement_type=settlement_type, contract_size=contract_size,
+                                                    point_value=point_value, tick_size=tick_size, quote_unit=quote_unit,
+                                                    asset_issuer_id=asset_issuer_id, asset_status=asset_status,
+                                                    display_name=display_name, currency=currency,
+                                                    underlying_asset_id=underlying_asset_id,
+                                                    description=description, country_id=country_id, venue_id=venue_id,
+                                                    comments=comments, links=links, references=references,
+                                                    issue_date=issue_date, expiry_date=expiry_date,
+                                                    *args, **kwargs)
