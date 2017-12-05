545e42998de64aecebf304085d0a731bb4879281
paul-rs <roland.paul.santos@gmail.com>
2017-12-05 19:20:21 +0800

    AMAAS-1186 Added missing enums for Demo accounts

--- amaascore/asset_managers/enums.py
+++ amaascore/asset_managers/enums.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 ASSET_MANAGER_TYPES = {'Accredited Investor', 'Bank', 'Broker', 'Corporate Treasury', 'Family Office',
                        'Fund Administrator', 'Fund Manager', 'Hedge Fund', 'Individual', 'Private Equity',
                        'Trust Fund', 'Venture Capital', 'Data Provider'}
-RELATIONSHIP_TYPES = {'Administrator', 'External', 'Front Office', 'Employee', 'Data Provider'}
-ACCOUNT_TYPES = {'Internal', 'Test', 'Basic', 'Professional'}
+RELATIONSHIP_TYPES = {'Administrator', 'External', 'Front Office', 'Employee', 'Data Provider', 'Demo'}
+ACCOUNT_TYPES = {'Internal', 'Test', 'Basic', 'Professional', 'Demo'}
