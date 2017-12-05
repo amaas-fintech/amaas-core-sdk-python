7a9d60d1999c5067ad959eef727af703031a5042
He Ye <keye906@yahoo.com>
2017-12-05 18:08:17 +0800

    fixed maturity date

--- amaascore/assets/foreign_exchange.py
+++ amaascore/assets/foreign_exchange.py
@@ -83,12 +83,12 @@ class ForeignExchangeSpot(ForeignExchangeBase):
     def __init__(self, asset_manager_id, asset_id, asset_status='Active', description='', country_codes=[],
                  underlying=None, settlement_date=None, currency=None,
                  display_name=None, *args, **kwargs):
-        self.underlying = underlying
-        self.settlement_date = settlement_date
         super(ForeignExchangeSpot, self).__init__(asset_manager_id=asset_manager_id, asset_id=asset_id,
                                                   asset_status=asset_status, currency=currency,
                                                   country_codes=country_codes, display_name=display_name,
                                                   description=description, *args, **kwargs)
+        self.underlying = underlying
+        self.settlement_date = settlement_date
 
     @property
     def settlement_date(self):
@@ -114,13 +114,15 @@ class ForeignExchangeForward(ForeignExchangeSpot):
     def __init__(self, asset_manager_id, asset_id, asset_status='Active', description='', country_codes=[],
                  forward_rate=None, underlying=None, settlement_date=None, fixing_date=None, currency=None,
                  display_name=None, *args, **kwargs):
-        self.forward_rate = forward_rate
-        self.fixing_date = fixing_date
         super(ForeignExchangeForward, self).__init__(asset_manager_id=asset_manager_id, asset_id=asset_id,
                                                      asset_status=asset_status, currency=currency,
                                                      underlying=underlying, settlement_date=settlement_date,
                                                      country_codes=country_codes, display_name=display_name,
                                                      description=description, *args, **kwargs)
+        self.forward_rate = forward_rate
+        self.fixing_date = fixing_date
+        self.settlement_date = settlement_date
+
 
     @property
     def settlement_date(self):
