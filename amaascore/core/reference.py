6a1b33e6e58e8a00aa7ed9eb8884af8d2b4add11
paul-rs <roland.paul.santos@gmail.com>
2017-12-11 10:24:20 +0800

    Fixing hashing differences

--- amaascore/core/reference.py
+++ amaascore/core/reference.py
@@ -9,3 +9,16 @@ class Reference(AMaaSModel):
         self.reference_value = reference_value
         self.reference_primary = reference_primary
         super(Reference, self).__init__(*args, **kwargs)
+
+    @property
+    def reference_primary(self):
+        if hasattr(self, '_reference_primary'):
+            return self._reference_primary
+
+    @reference_primary.setter
+    def reference_primary(self, value):
+        """
+        Always convert to bool if the service/database returns 0 or 1
+        """
+        if value is not None:
+            self._reference_primary = True if value else False
